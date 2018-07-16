955a265076795c849d9d89b19706e072b11f3e3e
alexwu1096 <41294267+alexwu1096@users.noreply.github.com>
2018-07-16 15:31:41 -0400

    first commit

---
+++ Assets/Scripts/ThroughPortal.cs
@@ -0,0 +1,29 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ThroughPortal : MonoBehaviour {
+	
+	public GameObject otherPortal;
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+	void OnTriggerEnter (Collider other) {
+		if (other.tag == "Player") {
+			other.transform.position = otherPortal.transform.position + otherPortal.transform.forward *-3;
+			other.GetComponent < Rigidbody > ().velocity = Vector3.zero;
+		}
+	}
+
+
+}
+
+
+
